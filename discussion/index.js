e2d630f337dd33b0f9a0c08abf0a54b3cd638ae7
StevenAgang06 <suagang@sscgi.com>
2025-02-27 12:59:33 +0800

    Initial Commit

--- discussion/index.js
+++ discussion/index.js
@@ -93,3 +93,133 @@ console.log(`The result of logical AND operator is ${allRequirements}`);
 
 let someRequirement = isRegistered || islegalAge;
 console.log(`The result of logical OR operator is ${someRequirement}`);
+
+// Functions
+// lines/block of codes that tell our device/application to perform
+// certain task when called/invoke
+// functions are create complicated tasks to run several lines of code in succession
+// tehy are also use to prevent repeating lines/blocks of codes that perform
+// the same task or function over and over again.
+
+// Function declarations - it defines a function with the specified parameters
+
+/*
+    Syntax:
+        function functionName(){
+            code block (statements)        
+        }
+
+        function keyword - used ti define a javascript functions
+        functionName - function are named to be able to use later in the code
+        function block ({}) - the statements inside of functions and comprose the body of the functions
+                                this is where the code to be executed
+*/
+
+function printName() {
+  console.log("My name is Juan");
+}
+
+//called/invoked
+printName(); // function declarations/invoker
+
+// Function invocation - the code block and statements inside a function
+// is not immediately executed when the function is defined.
+// The code block and the statements inside a function is executed when the function is invoke or called.
+
+// Function declarations vs expressions
+
+/*
+    Function declaration
+    
+    - a function can be created through function declaration by using the function keyword
+    and adding a function keyword and adding a function name
+
+    -declared functions are not executed immediately. They are 'saved for later use'
+    and will be executed later, when they are invoked (called upon).
+
+    Ex:
+
+        declaredFunction() - can be hoisted, as long as the function has been defined
+    
+*/
+
+greet();
+
+function greet() {
+  console.log("Hello");
+}
+
+// not hoisted
+// console.log(name);
+
+// let name = "Dave";
+
+// Function Expression
+/* 
+    A function can also stored in a variable. This is
+    called function expression.
+
+    AA function expression is an anonymous function assigned
+    to the variableFunction
+
+    Anonymous function - a function without a name
+*/
+
+let variableFunction = function () {
+  console.log("Hello again!");
+};
+
+variableFunction();
+
+// Function scoping
+/*
+    scope is the accessibility (visibility) of teh variables
+    within our program
+
+    Javascript variables has 3 types of scope:
+    1. local/block scope
+    2. global scope
+    3. function scope
+*/
+
+{
+  let localVar = "Scope Sample";
+  console.log(localVar);
+}
+
+let globalVar = "Global Variable";
+console.log(globalVar);
+
+function showNames() {
+  // Function Scope Variable
+  var functionVAr = "Function Scope";
+  const functionConst = "Const";
+  let functionLet = "Let";
+
+  //   console.log(functionVAr);
+  //   console.log(functionConst);
+  //   console.log(functionLet);
+}
+
+// Shows an error cause all the variable is out of scope
+// console.log(functionVAr);
+// console.log(functionConst);
+// console.log(functionLet);
+
+showNames();
+
+// Using alert() - it allows us to show small window at the top of the browser
+// page to show the information to our users as oppose to console.log() which only shows te message on the console.
+
+alert("WARNING! This is an alert");
+
+function showAlert() {
+  alert("Hello");
+}
+
+showAlert();
+
+// Using promt() - it allows us to show small window at the top of the browser to gather user input.
+
+let samplePrompt = prompt("Enter your username");
+console.log(`Hello your username is ${samplePrompt}`);
---
+++ discussion/so2_activity1.js
@@ -0,0 +1,24 @@
+// Creating an object
+const products = {
+  name: [
+    ["Nike", "Addidas", "Puma"],
+    ["Black T-shirt", "White T-shirt", "Red T-shirt"],
+    ["Black Pants", "Blue Jeans", "Red Pants"],
+  ],
+
+  price: [
+    [100, 150, 50],
+    [50, 75, 25],
+    [150, 60, 40],
+  ],
+
+  stock: [
+    [10, 15, 20],
+    [7, 4, 8],
+    [2, 9, 7],
+  ],
+
+  category: [["Shoes"], ["T-shirt"], ["Pants"]],
+};
+
+// Displaying all products and its information
